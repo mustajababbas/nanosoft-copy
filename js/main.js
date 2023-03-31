e28f252a77d36ed4f0c82d2a27b7c7af63d638e7
Syed Mustajab Abbas Nqvi <85496904+wawawasi@users.noreply.github.com>
2023-03-31 11:18:04 +0500

    new changes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,47 @@
 const year = document.querySelector ('#current-year');
 year.innerHTML = new Date().getFullYear();
+
+
+
+
+var worktabs = $('.worktabs');
+var selector = $('.worktabs').find('a').length;
+var activeItem = worktabs.find('.active');
+var activeWidth = activeItem.innerWidth();
+$(".selector").css({
+  "left": activeItem.position.left + "px", 
+  "width": activeWidth + "px"
+});
+
+$(".worktabs").on("click","a",function(e){
+  e.preventDefault();
+  $('.worktabs a').removeClass("active");
+  $(this).addClass('active');
+  var activeWidth = $(this).innerWidth();
+  var itemPos = $(this).position();
+  $(".selector").css({
+    "left":itemPos.left + "px", 
+    "width": activeWidth + "px"
+  });
+});
+
+
+$( function() {
+  var $grid = $('.grid').isotope({
+    itemSelector: 'article'
+  });
+
+  // filter buttons
+  $('.filters-button-group').on( 'click', 'a', function() {
+    var filterValue = $( this ).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+  $('.worktabs').each( function( i, worktabs ) {
+    var $worktabs = $( worktabs );
+    $worktabs.on( 'click', 'a', function() {
+      $worktabs.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+});
+
